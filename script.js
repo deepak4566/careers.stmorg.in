9b465308fc2e9b5930f4c9530adab4da6bd4b5bd
kiranpranay <pranaybannu12@gmail.com>
2023-08-24 01:31:27 +0530

    careers: add demo data
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ UTILITIES
 *************************************/
 // custom event polyfill
 (function () {
-  if (typeof window.CustomEvent === 'function') return false;
+  if (typeof window.CustomEvent === "function") return false;
 
   function CustomEvent(event, params) {
     params = params || { bubbles: false, cancelable: false, detail: undefined };
-    const evt = document.createEvent('CustomEvent');
-    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+    const evt = document.createEvent("CustomEvent");
+    evt.initCustomEvent(
+      event,
+      params.bubbles,
+      params.cancelable,
+      params.detail
+    );
     return evt;
   }
 
@@ -23,7 +28,9 @@ UTILITIES
     obj = obj || window;
     let running = false;
     const func = function () {
-      if (running) {return;}
+      if (running) {
+        return;
+      }
       running = true;
       requestAnimationFrame(() => {
         obj.dispatchEvent(new CustomEvent(name));
@@ -34,21 +41,27 @@ UTILITIES
   };
 
   /* init - you can init any event */
-  throttle('scroll', 'optimizedScroll');
+  throttle("scroll", "optimizedScroll");
 })();
 
 const getScrollXY = () => {
   let scrOfX = 0;
   let scrOfY = 0;
-  if (typeof window.pageYOffset === 'number') {
+  if (typeof window.pageYOffset === "number") {
     // Netscape compliant
     scrOfY = window.pageYOffset;
     scrOfX = window.pageXOffset;
-  } else if (document.body && (document.body.scrollLeft || document.body.scrollTop)) {
+  } else if (
+    document.body &&
+    (document.body.scrollLeft || document.body.scrollTop)
+  ) {
     // DOM compliant
     scrOfY = document.body.scrollTop;
     scrOfX = document.body.scrollLeft;
-  } else if (document.documentElement && (document.documentElement.scrollLeft || document.documentElement.scrollTop)) {
+  } else if (
+    document.documentElement &&
+    (document.documentElement.scrollLeft || document.documentElement.scrollTop)
+  ) {
     // IE6 standards compliant mode
     scrOfY = document.documentElement.scrollTop;
     scrOfX = document.documentElement.scrollLeft;
@@ -59,10 +72,13 @@ const getScrollXY = () => {
 const getDocHeight = () => {
   const D = document;
   return Math.max(
-  D.body.scrollHeight, D.documentElement.scrollHeight,
-  D.body.offsetHeight, D.documentElement.offsetHeight,
-  D.body.clientHeight, D.documentElement.clientHeight);
-
+    D.body.scrollHeight,
+    D.documentElement.scrollHeight,
+    D.body.offsetHeight,
+    D.documentElement.offsetHeight,
+    D.body.clientHeight,
+    D.documentElement.clientHeight
+  );
 };
 
 const isScrollBottom = (tolerance = 0) => {
@@ -85,57 +101,73 @@ const filteredGrid = {
   props: {
     items: {
       type: Array,
-      required: true },
+      required: true,
+    },
 
     emptyResult: {
       type: Object,
-      required: true },
+      required: true,
+    },
 
     query: {
       type: String,
-      default: '' },
+      default: "",
+    },
 
     filter: {
       type: String,
-      default: '' },
+      default: "",
+    },
 
     queryBy: {
       type: String,
-      default: '' },
+      default: "",
+    },
 
     filterBy: {
       type: String,
-      default: '' },
+      default: "",
+    },
 
     numResults: {
       type: Number,
-      default: 6 },
+      default: 6,
+    },
 
     initNumResults: {
       type: Number,
-      default: 6 },
+      default: 6,
+    },
 
     numCols: {
       type: Number,
-      default: 1 },
+      default: 1,
+    },
 
     infiniteDistance: {
       type: Number,
-      default: 600 } },
-
+      default: 600,
+    },
+  },
 
   computed: {
     results: function () {
-      const _results = this.items.filter(i => {
-        return (
-          (!this.queryBy ||
-          i[this.queryBy].toLowerCase().indexOf(this.query.toLowerCase()) !== -1) && (
-          !this.filterBy ||
-          i[this.filterBy].indexOf(this.filter) !== -1 || this.filter === ''));
-
-      }).slice(0, this.numResults);
+      const _results = this.items
+        .filter((i) => {
+          return (
+            (!this.queryBy ||
+              i[this.queryBy]
+                .toLowerCase()
+                .indexOf(this.query.toLowerCase()) !== -1) &&
+            (!this.filterBy ||
+              i[this.filterBy].indexOf(this.filter) !== -1 ||
+              this.filter === "")
+          );
+        })
+        .slice(0, this.numResults);
       return _results.length > 0 ? _results : this.emptyResult;
-    } },
+    },
+  },
 
   watch: {
     query: function (val, oldVal) {
@@ -143,11 +175,15 @@ const filteredGrid = {
     },
     filter: function (val, oldVal) {
       this.numResults = this.initNumResults;
-    } },
+    },
+  },
 
   created: function () {
-    window.addEventListener('optimizedScroll', () => {
-      if (isScrollBottom(this.infiniteDistance) && this.numResults < this.items.length) {
+    window.addEventListener("optimizedScroll", () => {
+      if (
+        isScrollBottom(this.infiniteDistance) &&
+        this.numResults < this.items.length
+      ) {
         this.numResults += this.numCols;
       }
     });
@@ -161,8 +197,8 @@ const filteredGrid = {
       v-bind:last="results.indexOf(i) === results.length - 1"
       v-bind:card-width="numCols === 1 ? '100%' : ((100/numCols) - 1.5) + '%'">
     </slot>
-  </transition-group>` };
-
+  </transition-group>`,
+};
 
 /*************************************
 COMPONENT: FILTER LIST
@@ -171,38 +207,46 @@ const filterList = {
   props: {
     activeFilter: {
       type: String,
-      default: '' },
+      default: "",
+    },
 
     allText: {
       type: String,
-      default: 'All' },
+      default: "All",
+    },
 
     backdrop: {
       type: Boolean,
-      default: true },
+      default: true,
+    },
 
     drawerOpen: {
       type: Boolean,
-      default: false },
+      default: false,
+    },
 
     filters: {
       type: Array,
-      required: true },
+      required: true,
+    },
 
     onChange: {
       type: Function,
-      required: true } },
-
+      required: true,
+    },
+  },
 
   methods: {
     toggleDrawer: function (bool) {
       this.drawerOpen = bool;
-    } },
+    },
+  },
 
   watch: {
     activeFilter: function (val, oldVal) {
       this.drawerOpen = false;
-    } },
+    },
+  },
 
   template: `<div class="gsf-filters" v-bind:class="{'gsf-filters-backdrop': backdrop, 'gsf-mobile-filters-open': drawerOpen}">
     <a v-on:click="toggleDrawer(!drawerOpen)" class="gsf-mobile-filters-drawer">
@@ -219,8 +263,8 @@ const filterList = {
           v-on:click="onChange(filter)"><span>{{filter}}</span></a></li>
       </ul>
     </div>
-  </div>` };
-
+  </div>`,
+};
 
 /*************************************
 COMPONENT: CAREER LISTING
@@ -229,56 +273,66 @@ const careerListing = {
   props: {
     cardWidth: {
       type: String,
-      default: '100%' },
+      default: "100%",
+    },
 
     data: {
       type: Object,
-      required: true },
+      required: true,
+    },
 
     last: {
       type: Boolean,
-      default: false } },
-
+      default: false,
+    },
+  },
 
   template: `<li class="gsf-career-listing" v-bind:class="{'gsf-career-listing-last': last}" v-bind:style="{width: cardWidth}">
     <a class="gsf-career-listing-inner" v-bind:href="data.url">
       <h2 class="gsf-career-title" v-html="data.title"></h2>
       <p class="gsf-career-description" v-html="data.summary"></p>
     </a>
-  </li>` };
-
+  </li>`,
+};
 
 /*************************************
 MAIN
 *************************************/
 const careers = {
   components: {
-    'filtered-grid': filteredGrid,
-    'filter-list': filterList,
-    'career-listing': careerListing },
+    "filtered-grid": filteredGrid,
+    "filter-list": filterList,
+    "career-listing": careerListing,
+  },
 
   props: {
     listings: {
       type: Array,
-      required: true },
+      required: true,
+    },
 
     categories: {
       type: Array,
-      required: true } },
-
+      required: true,
+    },
+  },
 
   data: () => ({
-    activeCategory: '',
-    noListings: [{
-      id: -1,
-      title: 'There are no current career listings that match your criteria.',
-      summary: '' }] }),
-
+    activeCategory: "",
+    noListings: [
+      {
+        id: -1,
+        title: "There are no current career listings that match your criteria.",
+        summary: "",
+      },
+    ],
+  }),
 
   methods: {
     setCategory: function (cat) {
       this.activeCategory = cat;
-    } },
+    },
+  },
 
   template: `<div class="careers-main">
     <filter-list
@@ -300,32 +354,32 @@ const careers = {
           v-bind:last="props.last"/>
       </template>
     </filtered-grid>
-  </div>` };
-
+  </div>`,
+};
 
 /*************************************
 GET DATA AND MOUNT
 *************************************/
-const careerListings = document.getElementById('career-listings');
-const listingData = JSON.parse(careerListings.getAttribute('data-listings'));
-const categoryData = JSON.parse(careerListings.getAttribute('data-categories'));
+const careerListings = document.getElementById("career-listings");
+const listingData = JSON.parse(careerListings.getAttribute("data-listings"));
+const categoryData = JSON.parse(careerListings.getAttribute("data-categories"));
 
 new Vue({
-  el: '#career-listings',
+  el: "#career-listings",
   render: function (createElement) {
     return createElement(careers, {
       props: {
-        listings: listingData.
-        map((e, i) => {
+        listings: listingData.map((e, i) => {
           return {
             id: i,
             title: e.title,
             summary: e.summary,
             url: e.url,
-            categories: e.categories };
-
+            categories: e.categories,
+          };
         }),
-        categories: categoryData } });
-
-
-  } });
+        categories: categoryData,
+      },
+    });
+  },
+});
